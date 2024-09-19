31059716eaf9cd7c4c89a77f61f43c6afc7a16d0
lokeshtheprogrammer <132331354+lokeshtheprogrammer@users.noreply.github.com>
2024-09-19 12:44:17 +0530

    Create  Find the largest element of an array.C

---
+++ Find the largest element of an array.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int max_element(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Largest element of the array is %d.\n", max_element(arr, n));
+    return 0;
+}
